d11da6982798e7947f9b97fd983867a35755bc8c
Raman Lenka <oracle.radharaman@gmail.com>
2024-04-08 12:00:53 +0000

    xy

---
+++ models/example/delete_duplicates.sql
@@ -0,0 +1,11 @@
+{{config (materialized = 'table')}}
+
+SELECT EmployeeID,FirstName, LastName, Department, Salary FROM
+(
+SELECT EmployeeID,FirstName, LastName, Department, Salary,
+ROW_NUMBER()
+OVER 
+(PARTITION BY EmployeeID
+ORDER BY EmployeeID) RNK
+FROM Employee
+) WHERE RNK>1
